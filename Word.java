82e27f3cc1b87e1f143186058240c2aa72f9dc8e
samibadra <samibadra@yahoo.com>
2015-05-21 20:46:06 -0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -6,6 +6,10 @@
  FILE: Word.java
  
  Copyright (c) 2015 Sami Badra. All rights reserved.
+ 
+ DISCLAIMER: Any unauthorized use, including but not limited to, copying or
+ redistributing any chunk of the source code (or an entire file) will result in
+ punishment by law. I, Sami Badra, own all rights to the files and their contents.
  */
 
 public class Word implements Comparable<Word> {

ca86fb5fb1a053a3cfcc1dd71eb5688028addf5e
samibadra <samibadra@yahoo.com>
2015-05-21 20:25:27 -0700

    Create word.java

---
+++ word.java
@@ -0,0 +1,50 @@
+/**
+ * @author samib
+ *
+ */
+public class Word implements Comparable<Word> {
+	private String s;
+	
+	/**
+	 * constructor for creating the word
+	 * @param s sets this word to the string passed in
+	 */
+	public Word(String s){
+		this.s = s;
+	}
+	
+	/**
+	 * gets the string of this word
+	 * @return the string of this word
+	 */
+	public String getS() {
+		return s;
+	}
+	
+	/**
+	 * sets the string of this word
+	 * @param s the string that we are setting this word to
+	 */
+	public void setS(String s) {
+		this.s = s;
+	}
+	
+	/**
+	 * compareTo method compares two strings to each other
+	 */
+	@Override
+	public int compareTo(Word w) {
+		String s1, s2;
+		s1 = this.s.replaceAll("\\W","");
+		s2 = w.getS().replaceAll("\\W","");
+		return s1.compareToIgnoreCase(s2);
+	}
+	
+	/**
+	 * toString method used when printing out the word object
+	 */
+	public String toString() {
+		return s;
+	}
+	
+}
